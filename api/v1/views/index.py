cf129be7c4934be292708ff2282c76f6abb2d2c5
Mikalach <mika.djetic@gmaiul.com>
2023-04-12 02:17:28 -0700

    added the module documentation so it works

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-
+"""
+module documented
+"""
 from flask import jsonify
 from api.v1.views import app_views
+
 
 @app_views.route('/status', methods=['GET'])
 def get_status():
