63c3c222aaa655d3ca197652d913042c3104682a
Meashyshy <camillefvrl@gmail.com>
2023-04-13 04:35:31 -0700

    Added user view

---
+++ api/v1/views/users.py
@@ -0,0 +1,65 @@
+#!/usr/bin/python3
+"""Defines the RESTful API actions for the user objects."""
+from flask import jsonify, abort, request
+from api.v1.views import app_views
+from models import storage
+from models.user import User
+
+
+@app_views.route('/users', methods=['GET'], strict_slashes=False)
+def get_all_users():
+    """Retrieves the list of all User objects."""
+    amenities = storage.all(User).values()
+    return jsonify([user.to_dict() for user in amenities])
+
+
+@app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
+def get_user(user_id):
+    """Retrieves a user object."""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    return jsonify(user.to_dict())
+
+
+@app_views.route('/users/<user_id>', methods=['DELETE'],
+                 strict_slashes=False)
+def delete_user(user_id):
+    """Deletes a user object."""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    user.delete()
+    storage.save()
+    return jsonify({})
+
+
+@app_views.route('/users', methods=['POST'], strict_slashes=False)
+def create_user():
+    """Creates a user object."""
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    name = data.get('name')
+    if name is None:
+        abort(400, 'Missing name')
+    user = user(**data)
+    user.save()
+    return jsonify(user.to_dict()), 201
+
+
+@app_views.route('users/<user_id>', methods=['PUT'], strict_slashes=False)
+def update_user(user_id):
+    """Updates a user object."""
+    user = storage.get(User, user_id)
+    if user is None:
+        abort(404)
+    data = request.get_json()
+    if data is None:
+        abort(400, 'Not a JSON')
+    ignore_keys = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(user, key, value)
+    user.save()
+    return jsonify(user.to_dict()), 200 
