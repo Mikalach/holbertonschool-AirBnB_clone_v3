e887f28b2483b01ad252160f637fdc1f4c077fbf
Mikalach <mika.djetic@gmaiul.com>
2023-04-14 01:46:20 -0700

    asked gpt for a little problem in my code

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -14,7 +14,8 @@ def get_all_places(city_id):
     city = storage.get(City, city_id)
     if city is None:
         abort(404)
-    return jsonify(storage.all(Place).values())
+    places = [place.to_dict() for place in city.places]
+    return jsonify(places)
 
 
 @app_views.route('/places/<place_id>', methods=['GET'], strict_slashes=False)
@@ -26,8 +27,7 @@ def get_place(place_id):
     return jsonify(place.to_dict())
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'],
-                 strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['DELETE'], strict_slashes=False)
 def delete_place(place_id):
     """Deletes a place object."""
     place = storage.get(Place, place_id)
@@ -35,7 +35,7 @@ def delete_place(place_id):
         abort(404)
     place.delete()
     storage.save()
-    return jsonify({})
+    return jsonify({}), 200
 
 
 @app_views.route('/cities/<city_id>/places', methods=['POST'], strict_slashes=False)
@@ -47,22 +47,23 @@ def create_place(city_id):
     data = request.get_json()
     if data is None:
         abort(400, 'Not a JSON')
-    id = data.get('user_id')
-    if id is None:
+    user_id = data.get('user_id')
+    if user_id is None:
         abort(400, 'Missing user_id')
-    if id is not isinstance(User):
+    user = storage.get(User, user_id)
+    if user is None:
         abort(404)
     name = data.get('name')
     if name is None:
         abort(400, 'Missing name')
     data['city_id'] = city_id
+    data['user_id'] = user_id
     place = Place(**data)
-    place.city_id = city_id
-    place.save
+    place.save()
     return jsonify(place.to_dict()), 201
 
 
-@app_views.route('places/<place_id>', methods=['PUT'], strict_slashes=False)
+@app_views.route('/places/<place_id>', methods=['PUT'], strict_slashes=False)
 def update_place(place_id):
     """Updates a place object."""
     place = storage.get(Place, place_id)
@@ -76,4 +77,4 @@ def update_place(place_id):
         if key not in ignore_keys:
             setattr(place, key, value)
     place.save()
-    return jsonify(place.to_dict()), 200 
+    return jsonify(place.to_dict()), 200
